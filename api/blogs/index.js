2824965f719c2304aaecd8660f811bda580d2045
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-12-18 16:13:32 +0530

    Added CORS polic

--- api/blogs/index.js
+++ api/blogs/index.js
@@ -1,40 +1,19 @@
-// import clientPromise from "../../lib/mongodb";
-
-// export default async function handler(req, res) {
-//   if (req.method !== "GET") {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db("ACM-SIGAI");
-//     const collection = db.collection("blogs");
-
-//     const blogs = await collection
-//       .find({})
-//       .project({
-//         image: 1,
-//         title: 1,
-//         authorName: 1,
-//         authorRole: 1,
-//         slug: 1,
-//         smallDescription: 1
-//       })
-//       .toArray();
-
-//     res.status(200).json(blogs);
-//   } catch (err) {
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// }
-
-
 // api/blogs/index.js
 import 'dotenv/config'; // loads .env.local automatically
 import clientPromise from "../../lib/mongodb.js";
 import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
+  // === CORS headers ===
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://acm-blogs.vercel.app/'); // allow frontend on localhost
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   if (req.method !== 'GET') {
     res.setHeader('Allow', ['GET']);
     return res.status(405).json({ error: 'Method not allowed' });
@@ -77,5 +56,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
-
-
